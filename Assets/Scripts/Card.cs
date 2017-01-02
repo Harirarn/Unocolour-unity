f8fae4ab9d99d39511f1b2a96846a0e44f45325a
K Hariram <harirarn@gmail.com>
2017-01-02 15:01:16 +0530

    Creation

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card : MonoBehaviour {
+
+	public enum Cardcolor{ccred, ccyellow, ccgreen, ccblue, ccwild, ccempty}
+
+	public List<Sprite> card_sprite;
+	public List<Sprite> card_hover_sprite;
+	public List<Sprite> card_inactive_sprite;
+
+	public Cardcolor color;
+	public int active;
+	public bool hover;
+
+	public int posx, posy, tilt;
+
+	public SpriteRenderer card_renderer;
+
+	public void Set_color(Cardcolor color){
+		card_renderer = GetComponent<SpriteRenderer> ();
+		this.color = color;
+		UpdateCard();
+	}
+
+	public void activate(){
+		active = 1;
+		UpdateCard ();
+	}
+	public void disable(){
+		active = 0;
+		UpdateCard ();
+	}
+	public void select_card(){
+		active = 2;
+		UpdateCard ();
+	}
+
+	public void UpdateCard(){
+		switch (active) {
+		case 0:
+			card_renderer.sprite = card_inactive_sprite [(int) color];
+			break;
+		case 1:
+			if (hover) {
+				card_renderer.sprite = card_hover_sprite [(int)color];
+			} else {
+				card_renderer.sprite = card_sprite [(int)color];
+			}
+			break;
+		default:
+			card_renderer.sprite = card_hover_sprite [(int)color];
+			break;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		card_renderer = GetComponent<SpriteRenderer> ();
+		active = 0;
+		hover = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Pile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pile : MonoBehaviour {
+
+	public List<Card> cardpile = new List<Card>();
+	public Card card;
+	public Card.Cardcolor topcard;
+
+	public int row, col;
+
+	public Pile (int row_, int col_){
+		row = row_;
+		col = col_;
+	}
+
+	public void activate(){
+		if (cardpile.Count > 0) {
+			cardpile [cardpile.Count - 1].activate ();
+		}
+	}
+	public void disable(){
+		if (cardpile.Count > 0) {
+			cardpile [cardpile.Count - 1].disable ();
+		}
+	}
+	public void select_card(){
+		if (cardpile.Count > 0) {
+			cardpile [cardpile.Count - 1].select_card ();
+		}
+	}
+
+
+	public void Add_card(Card.Cardcolor color){
+		disable ();
+		Card newcard = Instantiate (card, new Vector3 ( row*0.8f-3.6f, col-2.0f, -1), Quaternion.identity);
+		newcard.Set_color (color);
+		cardpile.Add (newcard);
+		topcard = color;
+	}
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
